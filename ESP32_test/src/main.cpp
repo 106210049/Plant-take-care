2a4d592cc7c02e0bd98ee19de02c23ae3ca3264d
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-10-01 22:26:12 +0700

    Add files via upload

---
+++ ESP32_test/src/main.cpp
@@ -0,0 +1,97 @@
+#include <Arduino.h>
+#include <math.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "DHTesp.h"
+class Sensor
+{
+private:
+  int DHT_PIN;
+  int soil_sensor;
+  DHTesp dhtSensor;
+  String temp, hum;
+
+public:
+  friend class Load;
+  friend class OLED_DISPLAY;
+  void Set_Sensor_Pin();
+  void Read_Sensor();
+};
+
+class Load
+{
+private:
+  // int Lamp_Pin;
+  // int Motor_Pin_Left;
+  // int Motor_Pin_Right;
+  int Buzzer;
+  int water_pump;
+
+public:
+  void
+  Set_Load_Pin();
+  void Turn_On_Pump();
+  void Turn_On_Buzzer();
+};
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
+#define ADDRESS 0x3C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+class OLED_DISPLAY
+{
+public:
+  void Set_up_oled();
+  void display_oled();
+};
+Sensor sensor;
+Load load;
+OLED_DISPLAY oled_1306;
+
+void Sensor::Set_Sensor_Pin()
+{
+  DHT_PIN = 2;
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+}
+void OLED_DISPLAY::Set_up_oled()
+{
+  // initialize OLED display with I2C address 0x3C
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, ADDRESS))
+  {
+    Serial.println(F("failed to start SSD1306 OLED"));
+    while (1)
+      ;
+  }
+  delay(2000);              // wait two seconds for initializing
+  oled.clearDisplay();      // clear display
+  oled.setTextSize(1);      // set text size
+  oled.setTextColor(WHITE); // set text color
+  oled.setCursor(0, 2);     // set position to display (x,y)
+}
+void OLED_DISPLAY::display_oled()
+{
+  oled.clearDisplay();
+  oled.setCursor(0, 2);
+
+  oled.println("Temperature: " + sensor.temp + "oC"); // set text
+  oled.println("Humidity: " + sensor.hum + "%");
+  oled.display(); // display on OLED
+}
+
+void Sensor::Read_Sensor()
+{
+  TempAndHumidity data = sensor.dhtSensor.getTempAndHumidity();
+  sensor.temp = String(data.temperature, 2);
+  sensor.hum = String(data.humidity, 1);
+}
+void setup()
+{
+  Serial.begin(9600);
+  sensor.Set_Sensor_Pin();
+  oled_1306.Set_up_oled();
+}
+void loop()
+{
+  sensor.Read_Sensor();
+  oled_1306.display_oled();
+}
